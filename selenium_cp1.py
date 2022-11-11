220cebdacb632bc8b78adf8df927d0538666fdcc
orange-hour <swhong0215@gmail.com>
2022-11-11 16:19:52 +0900

    selenium version 3.14.1 기준 코드 작성 시작

---
+++ selenium_cp1.py
@@ -0,0 +1,8 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+browser = webdriver.Chrome(executable_path="/Users/orangehour/Downloads/chromedriver")
+
+
